8b5e320c3ec5bfb45f7c66d4bf348d27affd65e8
adenord <alexandre.denord@gmail.com>
2023-10-11 22:22:53 +0200

    add launcher

---
+++ optimus_launcher.sh
@@ -0,0 +1,8 @@
+# gcc optimus_tester.c srcs/ft_printf.c srcs/handling_args.c srcs/nbr_args.c srcs/display_unsigned.c srcs/display_hexa.c srcs/display_address.c libft/libft.a -I includes/ && ./a.out
+gcc optimus_tester2.c srcs/*.c libft/libft.a -I includes
+./a.out > optimus2
+gcc optimus_tester1.c
+./a.out > optimus1
+xxd optimus1 > optimus1.hex
+xxd optimus2 > optimus2.hex
+diff optimus1.hex optimus2.hex
